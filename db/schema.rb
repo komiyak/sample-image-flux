22b8fc75ffd85fe760f3f86db2a9bbe4e78f73ea
Kosuke Komiya <kosuke.komiya@gmail.com>
2018-12-04 12:26:44 +0900

    User postgres instead of sqlite3.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_04_012219) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "username", limit: 16, null: false
     t.datetime "created_at", null: false
