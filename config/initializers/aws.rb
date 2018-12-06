5b26763579ce79e86038be003a153a57535621ff
Kosuke Komiya <kosuke.komiya@gmail.com>
2018-12-06 13:39:09 +0900

    Change the region.

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,7 +2,7 @@
 # https://devcenter.heroku.com/articles/direct-to-s3-image-uploads-in-rails
 Aws.config.update(
   {
-    region: 'us-east-1',
+    region: 'ap-northeast-1',
     credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
   })
 
