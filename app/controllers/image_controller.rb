1428377d539eff3a2d868fdfee880cfd7e058379
Kosuke Komiya <kosuke.komiya@gmail.com>
2018-12-06 16:31:34 +0900

    Upload a file to s3 directly.

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -6,6 +6,7 @@ class ImageController < ApplicationController
       data: {
         code: 'ok',
         url: @s3_direct_post.url,
+        host: URI.parse(@s3_direct_post.url).host,
         fields: @s3_direct_post.fields,
       }
     }
@@ -14,6 +15,12 @@ class ImageController < ApplicationController
   private
 
   def set_s3_direct_post
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+    @s3_direct_post = S3_BUCKET.presigned_post(
+      key: "uploads/#{SecureRandom.uuid}",
+      content_length_range: 0..10240,
+      acl: 'public-read',
+      metadata: {
+        'original-filename' => '${filename}'
+      })
   end
 end
